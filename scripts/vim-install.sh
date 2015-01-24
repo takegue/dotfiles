70b513e29d980009011a1f42c26cd4abbcf97ee9
TKNGUE <takeno@jnlp.org>
2015-01-24 16:54:01 +0900

    update configure codes
        update script to install vim

--- scripts/vim-install.sh
+++ scripts/vim-install.sh
@@ -1,8 +1,27 @@
 #!/bin/env sh
 
-hg clone https://vim.google.code /tmp
+
+if [ -e `which hg` ]; then
+    echo 'not found mercurial'
+    exit 1;
+fi 
+hg clone https://vim.googlecode.com/hg /tmp/vim
 
 cd /tmp/vim
 
+./configure \
+    --prefix=~/.local \
+    --enable-multibyte \
+    --enable-xim \
+    --enable-fontset \
+    --enable-luainterp \
+    --with-features=huge \
+    --disable-selinux \
+    --enable-luainterp=yes \
+    --with-lua-prefix=/usr/local/bin \
+    --enable-rubyinterp=yes \
+    --enable-pythoninterp=yes \
 
+make
+make install
 
